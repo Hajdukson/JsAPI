ad7cff71eacff0619bafd1d964b28a2d5d115b08
Michał Hajduk <michal.hajudk@gmail.com>
2021-10-07 19:24:14 +0200

    Add project files.

---
+++ JsAPI/Data/JsAPIContext.cs
@@ -0,0 +1,15 @@
+﻿using JsAPI.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace JsAPI.Data
+{
+    public class JsAPIContext : DbContext
+    {
+        public JsAPIContext (DbContextOptions<JsAPIContext> options)
+            : base(options)
+        {
+        }
+
+        public DbSet<Person> Person { get; set; }
+    }
+}
